d36e1b675e30f869ea42bbb8c6199544cb3681b6
Puneeth221 <puneethgopu04@gmail.com>
2026-09-03 14:41:50 +0530

    Add Perfect Squares program to check square status

---
+++ Perfect Squares.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Square {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int r = (int)Math.sqrt(n);
+
+        if (r * r == n)
+            System.out.println("Perfect Square");
+        else
+            System.out.println("Not Perfect Square");
+    }
+}
